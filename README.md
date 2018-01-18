82b7a7ba656e5b7007d95bdd30254616911223e8
Martyna <trejsu@gmail.com>
2018-01-18 14:36:45 +0100

    Initializes repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Schedules - coding assignment
+
+This project is a coding assignment based on scheduling problem. 
+Given a processing system that only accepts work as jobs, the goal is to
+schedule job execution to minimize the cost at any given point of time.
---
+++ src/main/java/com/schedules/SchedulesApplication.java
@@ -0,0 +1,12 @@
+package com.schedules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulesApplication.class, args);
+	}
+}
